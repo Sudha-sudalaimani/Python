dfed14271b1895428580011389654cffa62ff840
Sudha <s.sudha19082006@gmail.com>
2025-07-08 17:31:48 -0700

    Create string.py

---
+++ string.py
@@ -0,0 +1,13 @@
+#some string functions
+s1="Hello,World !"
+s2="Python"
+
+print(s1+s2)
+print(len(s1))
+print(s2.lower())
+print(s1.upper())
+print(s1.capitalize())
+print(s2.strip())
+print(s1.split(" "))
+words="T Am From CBE"
+print("-".join(words))
