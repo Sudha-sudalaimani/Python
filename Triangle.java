3f3e474c24497984cedaf6fa16f6bf290fbca550
Sudha <s.sudha19082006@gmail.com>
2025-09-12 21:14:25 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+rows=5
+for i in range(1,rows+1):
+    print(" "*(rows-i)+"* "*i)
+i=rows-1
+while(i>0):
+    print(" "*(rows-i)+"* "*i)
+    i=i-1
+
+"""
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+"""
