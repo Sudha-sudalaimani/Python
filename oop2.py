9d594777df6a0df6fd0a208c8b61b4778e21a724
Sudha <s.sudha19082006@gmail.com>
2025-10-02 18:40:20 +0530

    Create oop2.py

---
+++ oop2.py
@@ -0,0 +1,22 @@
+class Laptop:
+    price=0
+    processor=""
+    ram=""
+hp=Laptop()
+dell=Laptop()
+
+hp.price=50000
+hp.processor="i5U"
+hp.ram="16gb"
+
+dell.price=60000
+dell.processor="i5H"
+dell.ram="16Gb"
+
+print(hp.price)
+print(hp.processor)
+print(hp.ram)
+
+print(dell.price)
+print(dell.processor)
+print(dell.ram)
