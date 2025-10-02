df11b5a2cf0c0b5af3a1621b513459934cc7d723
Sudha <s.sudha19082006@gmail.com>
2025-10-02 18:37:22 +0530

    Create func_calc.py

---
+++ func_calc.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter a number: "))
+num2=int(input("Enter a number: "))
+def add():
+    print(num1+num2)
+def sub():
+    print(num1-num2)
+def mul():
+    print(num1*num2)
+def div():
+    print(num1/num2)
+
+add()
+sub()
+mul()
+div()
