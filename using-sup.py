89dcffd08e4d6b68f6a64815f3a7ffe029848218
Sudha <s.sudha19082006@gmail.com>
2025-10-03 16:59:57 +0530

    Create using-sup.py

---
+++ using-sup.py
@@ -0,0 +1,13 @@
+class person():
+    def __init__(self,name):
+        self.name=name
+        
+class student(person):
+    def __init__(self,grade,name):
+        super().__init__(name)
+        self.grade=grade
+    def display(self):
+        print("Name:",self.name)
+        print("Grade:",self.grade)
+s1=student("Sam","A+")
+s1.display()
