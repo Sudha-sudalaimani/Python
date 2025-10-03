f67d7c420059cba206bd6bb9aee1dbe438b9c85a
Sudha <s.sudha19082006@gmail.com>
2025-10-03 16:59:00 +0530

    Create poly.py

---
+++ poly.py
@@ -0,0 +1,16 @@
+#polymorphism
+class Animal():
+    def sound(self):
+        print("Animal Makes Sound")
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+class Bird(Animal):
+    def sound(self):
+        print("Birds Sing")
+a1=Animal()
+a1.sound()
+d1=Dog()
+d1.sound()
+b1=Bird()
+b1.sound()
