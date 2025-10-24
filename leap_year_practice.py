f1faf847043ced099edebfc29cac5a68be0fe637
TheDevJournal <s.sudha19082006@gmail.com>
2025-10-24 19:55:56 +0530

    Create leap_year_practice.py

---
+++ leap_year_practice.py
@@ -0,0 +1,11 @@
+year=int(input("Enter a year:"))
+if(year%4==0):
+    if(year%100):
+        if(year%400==0):
+            print(f"{year} is Leap Year")
+        else:
+            print(f"{year} is not Leap Year")
+    else:
+        print(f"{year} is a Leap Year")
+else:
+    print(f"{year} is Not Leap Year")
